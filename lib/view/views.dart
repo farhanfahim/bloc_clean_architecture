dcec31cdadcac0db6ffcb596400e15acce105d46
farhanfahim <dev.farhanfahim@gmail.com>
2024-11-28 16:51:37 +0500

    initial commit

---
+++ lib/view/views.dart
@@ -0,0 +1,10 @@
+// This file exports the following screens:
+// 1. LoginScreen
+// 2. SplashView
+// 3. HomeScreen
+
+export 'login/login_screen.dart'; // Exporting the LoginScreen widget from login/login_screen.dart
+
+export 'splash/splash_screen.dart'; // Exporting the SplashView widget from splash/splash_view.dart
+
+export 'home/home_screen.dart'; // Exporting the HomeScreen widget from movies/movies_screen.dart
